a2033b6a7460bbd0a223fa1aaa4c7fac54a6707a
Roman Sharf <pencilking2002@gmail.com>
2022-07-12 22:07:02 -0400

    - Makes projector ignore waypoint materials - Makes polyps spawn lower

--- Assets/Scripts/PolypSpawner.cs
+++ Assets/Scripts/PolypSpawner.cs
@@ -48,53 +48,9 @@ public class PolypSpawner : MonoBehaviour
 
     public void PositionPickup(PolypPickup pickup, Waypoint waypoint)
     {
-        // var randomVector = (new Vector3(0.001f, 0.001f, 0.001f) + Random.insideUnitSphere).normalized;
-        // randomVector.y *= 0.15f;
-        // pickup.transform.position = waypoint.transform.position + (Vector3.up) + randomVector;
         var player = GameManager.Instance.vrController;
         var spot = GameManager.Instance.GetPlaceSpot(waypoint);
         var playerDirection = (player.cam.transform.position - spot.transform.position).normalized;
-        pickup.transform.position = spot.transform.position + (Vector3.up * 0.5f) + (playerDirection * 0.5f);
-    }
-
-    // private void OnWaypointFilled(Waypoint waypoint)
-    // {
-    //     if (waypoint.GetWaypointIndex() != 0)
-    //     {
-    //         var pickup = GetPickup();
-    //         PositionPickup(pickup, waypoint);
-    //     }
-    // }
-
-    // private void OnPolypVoiceCueComplete(Waypoint waypoint)
-    // {
-    //     if (waypoint.GetWaypointIndex() == 0)
-    //     {
-    //         if (waypoint.currVoiceCue < waypoint.maxNumPolypPickups)
-    //         {
-    //             var pickup = GetPickup();
-    //             PositionPickup(pickup, waypoint);
-    //         }
-    //     }
-    //     else
-    //     {
-    //         if (waypoint.currVoiceCue < waypoint.maxNumPolypPickups)
-    //         {
-    //             var pickup = GetPickup();
-    //             PositionPickup(pickup, waypoint);
-    //         }
-    //     }
-    // }
-
-    private void OnEnable()
-    {
-        //EventManager.Player.onWaypointFilled += OnWaypointFilled;
-        //EventManager.Game.onPolypVoiceCueComplete += OnPolypVoiceCueComplete;
-    }
-
-    private void OnDisable()
-    {
-        //EventManager.Player.onWaypointFilled -= OnWaypointFilled;
-        //EventManager.Game.onPolypVoiceCueComplete -= OnPolypVoiceCueComplete;
+        pickup.transform.position = spot.transform.position + (Vector3.up * 0.25f) + (playerDirection * 0.5f);
     }
 }
